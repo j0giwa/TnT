76f3561c114edb139fe49ea34ff0b4a3f13b8c50
Jonas Schwind <77389555+j0giwa@users.noreply.github.com>
2024-01-25 22:28:53 +0100

    Initial Import

---
+++ src/test/java/de/thowl/notetilus/NotetilusApplicationTests.java
@@ -0,0 +1,13 @@
+package de.thowl.notetilus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotetilusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
