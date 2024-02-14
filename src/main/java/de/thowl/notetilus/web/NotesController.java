1511fab4df15a61d47de5c769f7f35805212b6e2
j0giwa <77389555+j0giwa@users.noreply.github.com>
2024-02-14 22:41:46 +0100

    Hallo, Anmelden
    Amazon uses this as a Placeholder when noone is logged in, an i thought
    it was funny, so we do it too.

--- src/main/java/de/thowl/notetilus/web/NotesController.java
+++ src/main/java/de/thowl/notetilus/web/NotesController.java
@@ -3,6 +3,7 @@ package de.thowl.notetilus.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.SessionAttribute;
@@ -20,12 +21,14 @@ public class NotesController {
 
 	@GetMapping("/u/{username}/notes")
 	public String showNotePage(@SessionAttribute(name = "token", required = false) AccessToken token,
-			@PathVariable("username") String username) {
+			@PathVariable("username") String username, Model model) {
 		log.info("entering showLoginPage (GET-Method: /login)");
 
 		// Prevent unauthrised access
 		if (!this.authsvc.validateSession(token, username))
 			throw new ForbiddenException("Unathorised access");
+
+		model.addAttribute("user", username);
 
 		return "notes";
 	}
