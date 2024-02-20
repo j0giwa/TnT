6f5595c91e58626cde6c0c9d07c1942d44399915
j0giwa <77389555+j0giwa@users.noreply.github.com>
2024-02-20 21:57:49 +0100

    Deleted db dependend tests

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,9 +3,3 @@ INSERT INTO
 VALUES
 	(1, 'user', false),
 	(2, 'admin', true);
-
---INSERT INTO
---   users(id, firstname, lastname, username, email, password, group_id)
---VALUES
---	(1, 'Rüdiger', 'Rüdiger', 'Ruediger', 'ruediger@thowl.de', '$2a$15$q/VCgMfG.XLG55DT2dgehOVFcAMgoV2iwgB7AA2yhzpk53E6uJ.La', 1),
---	(2, 'Ragnar', 'Lothbrock', 'Ragnar', 'ragnar@thowl.de', '$2y$15$zsSW05QfB1XpwL61lq9T9uhIHqZ7YzglIfwpFm8qd5WBl/nUXkj76', 1);
--- src/test/java/de/thowl/notetilus/core/TestAuthenticationService.java
+++ src/test/java/de/thowl/notetilus/core/TestAuthenticationService.java
@@ -1,18 +1,13 @@
 package de.thowl.notetilus.core;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import de.thowl.notetilus.core.exeptions.InvalidCredentialsException;
 import de.thowl.notetilus.core.services.AuthenticationService;
-import de.thowl.notetilus.storage.entities.AccessToken;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -21,64 +16,6 @@ class TestAuthenticationService {
 
 	@Autowired
 	private AuthenticationService authsvc;
-
-	/**
-	 * Testing if a login whith valid credentials returns an accesstoken
-	 */
-	@Test
-	void testLogin() {
-		log.info("entering Integration test testLogin");
-		AccessToken token;
-		try {
-			// NOTE: The password of the testuser is concidered insecure for our standards
-			token = this.authsvc.login("ruediger@thowl.de", "SuperSicher1234");
-			assertNotNull(token, "Token should not be NULL");
-			assertNotNull(token.getUsid(), "Session ID should not be NULL");
-			assertEquals(0, token.getUserId(), "Wrong UserID in session token, expected = 0");
-		} catch (InvalidCredentialsException e) {
-			fail("login with valid credentials should not throw an exception");
-		}
-	}
-
-	/**
-	 * Testing if a login whith invalid credentials returns nothing
-	 */
-	@Test
-	@SuppressWarnings("unused") // We are testing for an exeption.
-	void testLoginInvalidCredetials() {
-		log.info("entering Integration test testLoginInvalidCredetials");
-
-		try {
-			AccessToken token = this.authsvc.login(null, null);
-			fail("Token should be NULL, one or more inputs were NULL");
-		} catch (InvalidCredentialsException e) {
-		}
-
-		try {
-			AccessToken token = this.authsvc.login(null, "BringMe115");
-			fail("Token should be NULL, one or more inputs were NULL");
-		} catch (InvalidCredentialsException e) {
-		}
-
-		try {
-			AccessToken token = this.authsvc.login("ruediger@thowl.de", null);
-			fail("Token should be NULL, one or more inputs were NULL");
-		} catch (InvalidCredentialsException e) {
-		}
-
-		try {
-			AccessToken token = this.authsvc.login("ruediger@thowl.de", "BringMe115");
-			fail("Token should be NULL, wrong password");
-		} catch (InvalidCredentialsException e) {
-		}
-
-		try {
-			AccessToken token = this.authsvc.login("ivar@test.de", "BringMe115");
-			fail("Token should be NULL, user does not exist");
-		} catch (InvalidCredentialsException e) {
-		}
-
-	}
 
 	@Test
 	void testValidatePassword() {
@@ -105,23 +42,5 @@ class TestAuthenticationService {
 		assertTrue(this.authsvc.validateEmail("johndoe@test.org"),
 				"'johndoe@test.org' This should match the requirements");
 	}
-
-	// TODO: this test does not work, the code that is beeing tested does.
-	/*
-	 * @Test
-	 * void testRegister() {
-	 * log.info("entering test testRegister");
-	 * String uname = "test";
-	 * String passwd = "test123";
-	 * String email = "test@th-owl.de";
-	 * 
-	 * this.authsvc.register(uname, email, passwd, passwd);
-	 * 
-	 * User usr = this.users.findByUsername(uname);
-	 * 
-	 * assertNotNull(usr, "User resgistration failed");
-	 * assertEquals(uname, usr.getUsername(), "Wrong username");
-	 * assertEquals(email, usr.getEmail(), "Wrong Email");
-	 * }
-	 */
+	
 }
