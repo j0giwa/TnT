ca89f0be15dca51057f002391367e89206e3b679
j0giwa <77389555+j0giwa@users.noreply.github.com>
2024-02-25 22:52:39 +0100

    Fixed issue where User.toString throws an StackOverflow exeption

--- src/main/java/de/thowl/tnt/storage/entities/Group.java
+++ src/main/java/de/thowl/tnt/storage/entities/Group.java
@@ -21,6 +21,7 @@ package de.thowl.tnt.storage.entities;
 import java.util.List;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -38,7 +39,7 @@ public class Group {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 
-	@OneToMany(mappedBy = "group")
+	@OneToMany(mappedBy = "group", fetch = FetchType.EAGER)
 	public List<User> members;
 
 	@NotNull
---
+++ src/test/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO
+	groups(id, name, admin)
+VALUES
+	(1, 'user', false),
+	(2, 'admin', true);
