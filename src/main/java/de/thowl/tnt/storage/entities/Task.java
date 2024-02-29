42d6cbc3f894d35c23d3a953effa08969ce642d8
j0giwa <77389555+j0giwa@users.noreply.github.com>
2024-02-29 20:39:43 +0100

    fix: Fixed inheritance problem

--- src/main/java/de/thowl/tnt/storage/entities/Task.java
+++ src/main/java/de/thowl/tnt/storage/entities/Task.java
@@ -1,20 +1,36 @@
+/*
+ *  TnT, Todo's 'n' Texts
+ *  Copyright (C) 2023  <name of author>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package de.thowl.tnt.storage.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
-import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
 @Entity
 @Table(name = "Tasks")
-@NoArgsConstructor
 @RequiredArgsConstructor
 public class Task extends Entry {
 
 	@NotNull
 	@NonNull
-	private Priority priorty;
+	private Priority priority;
 
 }
