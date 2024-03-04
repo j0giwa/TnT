5f7b300f864f687210fe3ef2a73bacaf02962e52
WalterWendji <rahaman.dongmepi@stud.th-owl.de>
2024-03-04 10:24:06 +0100

    update hide/show password

---
+++ src/main/resources/static/js/passwd.js
@@ -0,0 +1,53 @@
+function passwordVisibility(){
+    var x = document.getElementById("passwd");
+    var y = document.getElementById("show");
+    var z = document.getElementById("hide");
+
+     if(x.type === 'password'){
+       x.type="text";
+       y.style.display="block";
+       z.style.display="none";
+     }
+      else{
+         x.type="password";
+         y.style.display="none";
+         z.style.display="block";
+        }
+
+}
+
+function passwordVisibility1(){
+    var x = document.getElementById("password");
+    var y = document.getElementById("show");
+    var z = document.getElementById("hide");
+
+     if(x.type === 'password'){
+       x.type="text";
+       y.style.display="block";
+       z.style.display="none";
+     }
+      else{
+         x.type="password";
+         y.style.display="none";
+         z.style.display="block";
+        }
+
+}
+
+function passwordVisibility2(){
+    var x = document.getElementById("password2");
+    var y = document.getElementById("show");
+    var z = document.getElementById("hide");
+
+     if(x.type === 'password'){
+       x.type="text";
+       y.style.display="block";
+       z.style.display="none";
+     }
+      else{
+         x.type="password";
+         y.style.display="none";
+         z.style.display="block";
+        }
+
+}
