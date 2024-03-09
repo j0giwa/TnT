b810689f376d6f4a1d21e5daefca3f03c4b26a67
j0giwa <77389555+j0giwa@users.noreply.github.com>
2024-03-09 15:44:09 +0100

    Added jpa classes for shared notes

--- src/main/java/de/thowl/tnt/core/ByteUtils.java
+++
@@ -1,9 +0,0 @@
-package de.thowl.tnt.core;
-
-import java.nio.charset.StandardCharsets;
-
-public class ByteUtils {
-	public static String byteArrayToString(byte[] byteArray) {
-		return new String(byteArray, StandardCharsets.UTF_8);
-	}
-}
