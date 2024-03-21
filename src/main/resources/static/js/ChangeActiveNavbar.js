e268e170237a1d6f1c47f04fd0a70d137099f741
WalterWendji <rahaman.dongmepi@stud.th-owl.de>
2024-03-21 14:35:52 +0100

    update

--- src/main/resources/static/js/ChangeActiveNavbar.js
+++ src/main/resources/static/js/ChangeActiveNavbar.js
@@ -1,12 +1,12 @@
-function changeActive(index) {
-    // Alle Navlinks auswählen und die Klasse "active" entfernen
-    var navlinks = document.getElementsByClassName("navlink");
-    for (var i = 0; i < navlinks.length; i++) {
-      navlinks[i].classList.remove("active");
-    }
-    
-    // Dem ausgewählten Link die Klasse "active" hinzufügen
-    navlinks[index-1].classList.add("active");
-}
-
-
+document.addEventListener("DOMContentLoaded", function() {
+  function changeActive(index) {
+      // Alle Navlinks auswählen und die Klasse "active" entfernen
+      var navlinks = document.getElementsByClassName("navlink");
+      for (var i = 0; i < navlinks.length; i++) {
+        navlinks[i].classList.remove("active");
+      }
+      
+      // Dem ausgewählten Link die Klasse "active" hinzufügen
+      navlinks[index-1].classList.add("active");
+  }
+});
